29f9622859d789dff4139281f783a44054e48eb0
superjijiji <451738366@qq.com>
2018-12-10 11:34:38 +0800

    hhh

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
-    public static void main(){
+    public static void main(string[] args){
         System.out.println("hello");
+        int i=0;
+        if(i>1){
+System.out.print("hhh");
+        }
     }
 }
